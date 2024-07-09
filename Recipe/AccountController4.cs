5902cc0b55487feaa782cef7e66ea852a77efa6a
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2024-07-09 10:46:13 +0300

    AccountController4.cs

---
+++ Recipe/AccountController4.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    // Інші методи...
+
+    [HttpGet]
+    public async Task<IActionResult> EditProfile()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return RedirectToAction("Login");
+        }
+
+        var model = new EditProfileViewModel
+        {
+            Email = user.Email,
+            UserName = user.UserName
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditProfile(EditProfileViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return RedirectToAction("Login");
+        }
+
+        user.Email = model.Email;
+        user.UserName = model.UserName;
+
+        var result = await _userManager.UpdateAsync(user);
+        if (result.Succeeded)
+        {
+            await _signInManager.RefreshSignInAsync(user);
+            return RedirectToAction("EditProfileConfirmation");
+        }
+
+        foreach (var error in result.Errors)
+        {
+            ModelState.AddModelError(string.Empty, error.Description);
+        }
+
+        return View(model);
+    }
+
+    public IActionResult EditProfileConfirmation()
+    {
+        return View();
+    }
+}
